f20d0a9536df6a296987ae48bcf9f8139ea6bee6
Alexander Bidenko <alexbidenko1998@gmail.com>
2021-11-08 15:22:41 +0300

    Update

---
+++ pages/login.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-form>
+    <v-container
+      cols="12"
+      sm="12"
+      md="12"
+      class="d-flex justify-center align-center flex-column"
+    >
+      <v-row>
+        <v-col>
+          <v-text-field
+            :email="login.email"
+            label="Email"
+            outlined
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field
+            :password="login.password"
+            label="Пароль"
+            outlined
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-btn depressed color="primary" @click="userLogin">Войти</v-btn>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data: () => ({
+    login: {
+      email: '',
+      password: '',
+    },
+  }),
+  methods: {
+    async userLogin() {
+      const { email, password } = this.login
+      try {
+        const response = await this.$axios.$post('', this.login)
+        if (email !== '' && password !== '') {
+          return null
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+}
+</script>
---
+++ pages/registration.vue
@@ -0,0 +1,141 @@
+<template>
+  <v-form>
+    <v-container
+      cols="12"
+      sm="12"
+      md="12"
+      class="d-flex justify-center align-center flex-column"
+    >
+      <v-row>
+        <v-col>
+          <v-text-field :name="registration.name" label="Имя" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field :surname="registration.surname" label="Фамилия" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field
+            :patronymic="registration.patronymic"
+            label="Отчество"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-select
+            v-model="typeUserValue"
+            :items="items"
+            label="Тип пользователя ?"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field :email="registration.email" label="Email" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field :password="registration.password" label="Пароль" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-menu
+            ref="menu"
+            v-model="menu"
+            :close-on-content-click="false"
+            :return-value.sync="date"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template #activator="{ on, attrs }">
+              <v-text-field
+                v-model="date"
+                label="Picker in menu"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              />
+            </template>
+            <v-date-picker v-model="date" no-title scrollable>
+              <v-spacer />
+              <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+              <v-btn text color="primary" @click="$refs.menu.save(date)">
+                OK
+              </v-btn>
+            </v-date-picker>
+          </v-menu>
+        </v-col>
+      </v-row>
+      <v-btn depressed color="primary" @click="registrationInfo"
+        >Зарегистрироваться</v-btn
+      >
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'Registration',
+  data: () => {
+    const dateCalendar = new Date().toISOString().substr(0, 10)
+    return {
+      date: dateCalendar,
+      items: [
+        'Обучающийся',
+        'Представитель учебного заведения',
+        'Представитель ИТ компании',
+      ],
+      test: '',
+      menu: false,
+      typeUserValue: '',
+      registration: {
+        name: '',
+        surname: '',
+        patronymic: '',
+        typeUser: '',
+        email: '',
+        password: '',
+        dateRegistration: dateCalendar,
+      },
+    }
+  },
+  methods: {
+    async registrationInfo() {
+      const {
+        name,
+        surname,
+        email,
+        password,
+        typeUser,
+        dateRegistration,
+        patronymic,
+      } = this.registration
+      try {
+        const response = await this.$axios.$post('', this.registration)
+        if (
+          name !== '' &&
+          surname !== '' &&
+          patronymic !== '' &&
+          email !== '' &&
+          typeUser !== '' &&
+          email !== '' &&
+          dateRegistration !== '' &&
+          password !== ''
+        ) {
+          return null
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+}
+</script>
